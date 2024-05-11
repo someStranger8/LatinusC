e4591321b8200a10342022e462c009ca988311a1
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-05-10 21:20:22 -0400

    Update and rename deutsch.h to latinus.h

---
+++ src/latinus.h
@@ -0,0 +1,15 @@
+// Text replacement macros which replaces keywords in C with latin words
+
+#ifndef LATINUS_H
+    #define LATINUS_H
+    
+    // control flow keywords
+    #include "latinus/controlflow.h"
+
+    // misc keywords
+    #include "latinus/misc.h"
+
+    // datatype keywords
+    #include "latinus/types.h"
+
+#endif // LATINUS_H
