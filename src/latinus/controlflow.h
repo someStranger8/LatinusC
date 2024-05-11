b178a367947e0826968da0dd5aa0b77f80da2962
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-05-10 21:31:04 -0400

    Update and rename controlflow.h to controlflow.h

---
+++ src/latinus/controlflow.h
@@ -0,0 +1,59 @@
+// macro definition for control flow keywords translated to german
+// makrodefinition fur kontrollflussschlusselworter ins deutsche ubersetzt
+
+#ifndef LATINUS_CONTROLFLOW_H
+    #define LATINUS_CONTROLFLOW_H
+
+    #ifndef rumpo
+        #define rumpo break
+    #endif
+    
+    #ifndef exemplum
+        #define exemplum case
+    #endif
+    
+    #ifndef pergo
+        #define pergo continue
+    #endif
+    
+    #ifndef norma
+        #define norma default
+    #endif
+    
+    #ifndef effecio
+        #define effecio do
+    #endif
+    
+    #ifndef aliter
+        #define aliter else
+    #endif
+    
+    #ifndef itero
+        #define itero for
+    #endif
+    
+    #ifndef abeo
+        #define abeo goto
+    #endif
+    
+    #ifndef si
+        #define si if
+    #endif
+
+    #ifndef reditus
+        #define reditus return
+    #endif
+
+    #ifndef flecto
+        #define flecto switch
+    #endif
+
+    #ifndef nonsignum
+        #define nonsignum unsigned
+    #endif
+
+    #ifndef signum
+        #define signum signed
+    #endif
+
+#endif // LATINUS_CONTROLFLOW_H
