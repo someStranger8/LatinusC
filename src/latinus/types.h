07ef6151d943ddee3e2b2e12de35b6dc0f6c8ee4
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-05-11 09:39:49 -0400

    Update and rename types.h to types.h

---
+++ src/latinus/types.h
@@ -0,0 +1,48 @@
+// macro definition for datatypes translated to german
+// makrodefinition fur datentypen ins deutsche ubersetzt
+
+#ifndef LATINUS_TYPES_H
+    #define LATINUS_TYPES_H
+
+    #ifndef constans
+        #define constans const
+    #endif
+
+    #ifndef necessario
+        #define necessario auto
+    #endif
+
+    #ifndef numerus
+        #define numerus int
+    #endif
+
+    #ifndef littera
+        #define littera char
+    #endif
+
+    #ifndef inno
+        #define inno float
+    #endif
+
+    #ifndef duplus
+        #define duplus double
+    #endif 
+
+    #ifndef brevis
+        #define brevis short
+    #endif
+
+    #ifndef longus
+        #define longus long
+    #endif
+
+    #ifndef structura
+        #define structura struct
+    #endif
+
+    #ifndef vacuus
+        #define vacuus void
+    #endif
+    
+
+#endif // LATINUS_TYPES_H
