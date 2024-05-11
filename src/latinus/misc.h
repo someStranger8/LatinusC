169ae066dbed6b7cd19c5c2d0e30bd626b61d2b9
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-05-11 09:50:59 -0400

    Update and rename misc.h to misc.h

---
+++ src/latinus/misc.h
@@ -0,0 +1,37 @@
+// macro definition for miscellaneous keywords translated to latin
+
+#ifndef LATINUS_MISC_H
+    #define LATINUS_MISC_H
+    
+    #ifndef laevis
+        #define laevis volatile
+    #endif
+    
+    #ifndef primus
+        #define primus main
+    #endif
+
+    #ifndef est
+        #define est =
+    #endif
+
+    #ifndef est_parilis
+        #define est_parilis ==
+    #endif
+
+    #ifndef addo
+        #define addo +
+    #endif
+
+    #ifndef demo
+        #define demo -
+    #endif
+
+    #ifndef multiplico
+        #define multiplico *
+    #endif
+
+    #ifndef divido
+        #define divido /
+    #endif
+#endif
